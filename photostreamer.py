04fc9c97a18327bf77c344b8f287ca805d5ddb39
Dorian Fraser-Moore <dorian+code@theusefularts.org>
2020-12-19 21:26:56 +0000

    Added functions for web server

--- photostreamer.py
+++ photostreamer.py
@@ -17,3 +17,10 @@ def find_files(directory, types):
         for path in Path(directory).rglob(glob):
             files.append(path.absolute().as_posix())
     return files
+
+def image_selection(offset, length=100):
+    paths = find_image_files(processed_dir())
+    return (paths[offset:length], len(paths))
+
+def web_local_path(path):
+    return path.replace(processed_dir(),'');
